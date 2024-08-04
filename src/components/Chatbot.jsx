1bcce7633affc4559e8e04511e640ad8da789d84
nanduaji <nanduaji64@gmail.com>
2024-08-04 08:45:21 +0530

    removed the last change

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -75,7 +75,7 @@ const Chatbot = () => {
     return "Sorry, I don't understand.";
   };
 
-  const toggleChatbox1 = () => {
+  const toggleChatbox = () => {
     setIsChatOpen(!isChatOpen);
   };
 
@@ -116,13 +116,13 @@ const Chatbot = () => {
             />
             <button onClick={handleSendMessage}>Send</button>
           </div>
-          <button className={styles.closeButton} onClick={toggleChatbox1}>
+          <button className={styles.closeButton} onClick={toggleChatbox}>
             &times;
           </button>
         </div>
       ) : (
         <Tooltip title="Talk To Me">
-          <div className={styles.avatar} onClick={toggleChatbox1}>
+          <div className={styles.avatar} onClick={toggleChatbox}>
             <img src="avatar.jpg" alt="Chatbot Avatar" />
           </div>
         </Tooltip>
